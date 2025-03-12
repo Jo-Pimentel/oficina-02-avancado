45a199476cb1f41cb6b5699a41fea752c62e048c
Jo-Pimentel <joaopodolsky59@gmail.com>
2025-03-12 18:44:35 -0300

    Ciração das páginas de boletim.

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,31 +2,31 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-    /* const primeiro_aluno = await prisma.aluno.create({
-        data: {
-            nome: 'João Lucas dos Santos Pimentel',
-            email: 'joaoLucas@gmail.com',
-            idade: 999,
-        },
-    });
+    // const primeiro_aluno = await prisma.aluno.create({
+    //     data: {
+    //         nome: 'João Lucas dos Santos Pimentel',
+    //         email: 'joaoLucas@gmail.com',
+    //         idade: 999,
+    //     },
+    // });
 
-    const segundo_aluno = await prisma.aluno.create({
-        data: {
-            nome: 'Pedro da Silva Souza',
-            email: 'pedro@gmail.com',
-            idade: 1023,
-        },
-    });
+    // const segundo_aluno = await prisma.aluno.create({
+    //     data: {
+    //         nome: 'Pedro da Silva Souza',
+    //         email: 'pedro@gmail.com',
+    //         idade: 1023,
+    //     },
+    // });
 
-    const terceiro_aluno = await prisma.aluno.create({
-        data: {
-            nome: 'Henrique Oliveira',
-            email: 'henrique@gmail.com',
-            idade: 6079,
-        },
-    });
+    // const terceiro_aluno = await prisma.aluno.create({
+    //     data: {
+    //         nome: 'Henrique Oliveira',
+    //         email: 'henrique@gmail.com',
+    //         idade: 6079,
+    //     },
+    // });
 
-    console.log('Alunos inseridos: ', { primeiro_aluno, segundo_aluno, terceiro_aluno });*/
+    // console.log('Alunos inseridos: ', { primeiro_aluno, segundo_aluno, terceiro_aluno });
 
     const primeiro_professor = await prisma.professor.create ({
         data: {
--- server.js
+++ server.js
@@ -63,6 +63,7 @@ app.get('/', async (req, res) => {
             </table> <br>
             <button onclick="location.href='/novo-aluno'"> Adicionar novo aluno </button><br><br>
             <button onclick="location.href='/professores'"> Lista de professores </button><br><br>
+            <button onclick="location.href='/boletim'"> Boletim </button>
         </body>
         </html>`;
 
@@ -281,6 +282,150 @@ app.post('/novo-professor', async (req, res) => {
     } catch(erro) {
         console.error(erro);
         res.status(400).send("Erro ao remover o professor do servidor.");
+    }
+});
+
+app.get("/boletim", async (req, res) => {
+    try{
+        const boletins = await prisma.boletim.findMany();
+
+        let html = `
+        <!DOCTYPE html>
+            <html lang="pt-br">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title> Lista de professores </title>
+                    <style>
+                        body{
+                            margin: 0;
+                            padding: 0;
+                            font-family: Berlin Sans FB;
+                        }
+
+                        table, th, tr, td {
+                            border: solid 4px;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <table align="center">
+                        <th colspan="5"> Lista dos professores da escola </th>
+
+                        <tr>
+                            <th> ID do aluno </th>
+                            <th> nota_matematica </th>
+                            <th> nota_portugues </th>
+                            <th> nota_geografia </th>
+                            <th> nota_historia </th>
+                            <th> nota_biologia </th>
+                        </tr>`;
+                        
+        boletins.forEach(boletim => {
+            html += `
+            <tr>
+                <td> ${boletim.aluno_id} </td>
+                <td> ${boletim.nota_matematica} </td>
+                <td> ${boletim.nota_portugues} </td>
+                <td> ${boletim.nota_geografia} </td>
+                <td> ${boletim.nota_historia} </td>
+                <td> ${boletim.nota_biologia} </td>
+                <td>
+                    <form action="/remover-notas" method="POST" style="display:inline;">
+                        <input type="hidden" name="id" value="${boletim.id}">
+                        <button type="submit"> Remover </button>
+                    </form>
+                </td>
+            </tr>`;
+        });
+
+        html += `
+            </table> <br>
+            <button onclick="location.href='/novas-notas'"> Adicionar novas notas </button><br><br>
+            <button onclick="location.href='/'"> Voltar para a lista de alunos </button><br><br>
+        </body>
+        </html>`;
+    res.send(html);
+
+    } catch (error) {
+        console.error(error);
+        res.status(200).send('Erro ao buscar dados do boletim.');
+    };
+});
+
+app.get('/novas-notas', (req, res) => {
+    const html = `
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Inserir novos dados </title>
+        <style>
+            body {
+                font-family: Berlin Sans FB;
+            }
+            .formulario-novo-aluno {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="div-do-h1">
+            <h1 align="center"> Inserção de novos dados para alunos </h1>
+        </div>
+        <div class="formulario-novo-aluno">
+            <form action="/novas-notas" method="post">
+                <input type="number" id="nota_matematica" placeholder="nota de matemática" name="nota_matematica" required><br><br>
+                <input type="number" id="nota_portugues" placeholder="nota de portugues" name="nota_portugues" required><br><br>
+                <input type="number" id="nota_geografia" placeholder="nota de geografia" name="nota_geografia" required><br><br>
+                <input type="number" id="nota_historia" placeholder="nota de história" name="nota_historia" required><br><br>
+                <input type="number" id="nota_biologia" placeholder="nota de biologia" name="nota_biologia" required><br><br>
+
+                <button type="submit"> Adcionar notas </button>
+            </form><br>
+
+            <button onclick="location.href='/boletim'"> Retornar ao boletim </button>
+        </div>
+    </body>
+    </html>`;
+
+    res.send(html);
+});
+
+app.post('/novas-notas', async (req, res) => {
+    const { nota_matematica, nota_portugues, nota_geografia, nota_historia, nota_biologia } = req.body;
+    try {
+      await prisma.boletim.create({
+        data: {
+          nota_matematica: parseInt(nota_matematica),
+          nota_portugues: parseInt(nota_portugues),
+          nota_geografia: parseInt(nota_geografia),
+          nota_historia: parseInt(nota_historia),
+          nota_biologia: parseInt(nota_biologia),
+        },
+      });
+      
+      res.redirect('/boletim');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao adicionar as notas.');
+    }
+  });
+
+  app.post('/remover-notas', async(req, res) => {
+    const { aluno_id } = req.body;
+    try{
+        await prisma.boletim.delete({
+            where: { id: parseInt(id) }
+        });
+
+        res.redirect('/boletim');
+    } catch(erro) {
+        console.error(erro);
+        res.status(400).send("Erro ao remover as notas.");
     }
 });
 
